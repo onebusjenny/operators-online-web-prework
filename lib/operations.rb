e5acc7f8a212bdd1cfe435dcb1cc533fcfb84249
Jenny Chu <jchudesign0930@gmail.com>
2019-02-03 08:01:23 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,20 @@
 def unsafe?(speed)
-
+  
+  if speed > 60 
+    "true"
+  if speed < 40
+    "true"
+  if speed 
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed > 60
+	  "true"
+	 if speed < 40
+	   "true"
+	 if speed 
 end
 	
 
